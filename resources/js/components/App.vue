8ee10122e912707c18f4ec88d02512ace29a553d
Мурад Расулов <rassulovmurad@gmail.com>
2021-09-19 13:27:24 +0200

    test

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -14,10 +14,12 @@
             console.log( 'Component App mounted' );
 
             axios
-                .get( 'http://amodashboard/api' + this.$route.path )
+                .get( 'https://www.hub.integrat.pro/Murad/amodashboard/api' + this.$route.path )
                 .then(
                     response => ( this.amo_data = response.data )
                 );
+
+                //http://amodashboard/
 
             console.log( 'route' );
             console.log( this.$route );
